88d99f78317b1c8a9eed573aadf9be0f7bf0f034
kot <kot@kot-PC>
2014-12-12 01:45:40 +0400

    first

--- frontend/views/layouts/parts/_pre_header.php
+++ frontend/views/layouts/parts/_pre_header.php
@@ -16,7 +16,7 @@ $this->registerJsFile('/js/jquery.js', ['depends' => AppAsset::className()]);
 $this->registerJsFile('/js/jquery.easing.1.3.js', ['depends' => AppAsset::className()]);
 $this->registerJsFile('/js/camera.js', ['depends' => AppAsset::className()]);
 $this->registerJsFile('/js/jquery.ui.totop.js', ['depends' => AppAsset::className()]);
-//$this->registerJsFile('/js/jquery.mobile.customized.min.js', ['depends' => AppAsset::className()]);
+$this->registerJsFile('/js/jquery.mobile.customized.min.js', ['depends' => AppAsset::className()]);
 $this->registerJsFile('/js/bootstrap.js', ['depends' => AppAsset::className()]);
 $this->registerJsFile('/js/custom/index.js', ['depends' => AppAsset::className()]);
 $this->registerJsFile('/js/superfish.js', ['depends' => AppAsset::className()]);
@@ -24,7 +24,7 @@ $this->registerJsFile('/js/custom/header.js', ['depends' => AppAsset::className(
 
 
 //$this->registerJsFile('/js/forms.js', ['depends' => AppAsset::className()]);
-//$this->registerJsFile('/js/jquery.cokie.js', ['depends' => AppAsset::className()]);
+$this->registerJsFile('/js/jquery.cookie.js', ['depends' => AppAsset::className()]);
 
 ?>
 
